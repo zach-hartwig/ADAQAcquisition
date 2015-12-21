c1025fa68f94c017911ac5bf6738befc33966a26
Zachary S Hartwig <hartwig@psfc.mit.edu>
2015-12-20 20:44:27 -0500

    Fixing bug in setup.sh script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -17,7 +17,7 @@ else
     SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
     export ADAQACQUISITION_HOME=${SCRIPTDIR///scripts}
     export PATH=${SCRIPTDIR///scripts/}/bin:$PATH
-    export LD_LIBRARY_PATH=$ADAQHOME/lib/$HOSTTYPE:$ADAQACQUISITION_HOME/bin:$$LD_LIBRARY_PATH
+    export LD_LIBRARY_PATH=$ADAQHOME/lib/$HOSTTYPE:$ADAQACQUISITION_HOME/bin:$LD_LIBRARY_PATH
     export PYTHONPATH=$ADAQHOME/lib/$HOSTTYPE:$PYTHONPATH
     
     echo -e "\nADAQAcquisition : The environment has been successfully configured!\n"
